0ad801770bfc7db05a49c3bdc7fd2bdb8a1f5997
Dilhara-Mallikarachchi <dilharamallikarachchi@gmail.com>
2025-04-26 22:43:38 +0530

    Add Replay game

--- guessing_game.py
+++ guessing_game.py
@@ -1,12 +1,22 @@
-import random 
-number = random.randint(1, 100) 
-print("Guess a number between 1 and 100") 
-guess = int(input()) 
-if guess == number: 
-print("You win!") 
-else: 
-print(f"Wrong! The number was {number}") 
-if guess < number: 
-    print("Too low!") 
-elif guess > number: 
-    print("Too high!")
+import random
+
+while True:
+    number = random.randint(1, 10)
+    print("Guess a number between 1 and 10")
+    guess = int(input())
+
+    
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+
+    
+    if guess == number:
+        print("You win!")
+    else:
+        print(f"Wrong! The number was {number}")
+
+    print("Play again? (y/n)")
+    if input().lower() != 'y':
+        break
